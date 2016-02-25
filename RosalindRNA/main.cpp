e15d0df9b3af9dd90ac0492f05d44530d0f344d0
Steven Leblanc <magus.arkiln@gmail.com>
2016-02-25 12:30:42 -0500

    Rosalind RNA
    rosalind.info/problems/rna

---
+++ RosalindRNA/main.cpp
@@ -0,0 +1,37 @@
+/*
+An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+
+Given a DNA string tt corresponding to a coding strand, its transcribed RNA string uu is formed by replacing all occurrences of 'T' in tt with 'U' in uu.
+
+Given: A DNA string tt having length at most 1000 nt.
+
+Return: The transcribed RNA string of tt.
+
+Taken from rosalind.info/problems/rna
+*/
+
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string sequence;
+
+	ifstream input("rosalind_rna.txt");
+
+	if (input.is_open()) {
+		getline(input, sequence);
+
+		for (unsigned __int16 i = 0; i < sequence.size(); i++) {
+			if (sequence[i] == 'T') {
+				sequence[i] = 'U';
+			}
+		}
+	}
+
+	ofstream output("output.txt");
+
+	output << sequence;
+}
